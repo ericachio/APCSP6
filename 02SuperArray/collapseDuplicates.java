9703b6d3ebd63a9e52b937b94925e411bda92e67
Erica <ericanewyork@gmail.com>
2014-11-13 17:44:05 -0500

    okay

---
+++ 02SuperArray/collapseDuplicates.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class collapseDuplicates{
+    
+    public collapseDuplicates(ArrayList<Integer> L){
+	//L = new ArrayList<Integer>();
+	for (int i = 0; L.length-1; i++){
+	    if (L[i] = L[i+1]){
+		
+	    }
+	} 
+    }
+}
