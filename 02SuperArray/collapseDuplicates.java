34d4a400617c63b900ce937c847887740bf60cf7
Erica <ericanewyork@gmail.com>
2014-11-13 18:14:20 -0500

    okay

--- 02SuperArray/collapseDuplicates.java
+++ 02SuperArray/collapseDuplicates.java
@@ -3,11 +3,29 @@ import java.util.ArrayList;
 public class collapseDuplicates{
     
     public collapseDuplicates(ArrayList<Integer> L){
-	//L = new ArrayList<Integer>();
-	for (int i = 0; L.length-1; i++){
-	    if (L[i] = L[i+1]){
-		
+	for (int i = 1; i < L.size() - 1; i++){
+	    if (L.get(i) == L.get(i+1)){
+		L.remove(i+1);
+		collapseDuplicates trial = new collapseDuplicates(L);
 	    }
-	} 
+	}
+    }
+    
+    public static void main (String[]args){
+	ArrayList<Integer> H = new ArrayList<Integer>();
+	H.add(2);
+	H.add(4);
+	H.add(4);
+	H.add(4);
+	H.add(4);
+	H.add(5);
+	H.add(5);
+	H.add(2);
+	H.add(4);
+	H.add(3);
+	System.out.println(H);
+	collapseDuplicates yo = new collapseDuplicates(H);
+	System.out.println(H);
+	
     }
 }
