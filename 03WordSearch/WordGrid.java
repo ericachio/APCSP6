28a56ed59a6c918798520f3d1e31de1b43ca51a9
Erica <ericanewyork@gmail.com>
2014-11-19 12:34:34 -0500

    new file

---
+++ 03WordSearch/WordGrid.java
@@ -0,0 +1,15 @@
+public class WordGrid{
+    private char[][]data;
+
+    public WordGrid(int rows, int cols){
+	data = new data[rows][cols];
+    }
+    private void clear(){
+    }
+    public String toString(){
+    }
+    public boolean addWordHorizontal(String word, int row, int col){
+    }
+    
+    public 
+}
