cccc47e590d754f4d7a4a5edb9767abb1376a27f
Erica <ericanewyork@gmail.com>
2014-12-01 12:30:29 -0500

    work done in class

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,5 +1,20 @@
 import java.util.Random;
 
 public class OrderedSuperArray extends SuperArray{
+    int elements;
+    public static String[] superArray;
 
+    public void sort(String[] array){
+	int length = array.length;
+	String temp = new String[length];
+	for (int i = 0; i < length; i ++){
+	    if (temp[i].comparesTo(temp[i+1] > 0)){
+		String t;
+		t = temp[i];
+		for (j = i; j < length; j ++){
+		    temp[i] = temp[i+1];
+		}
+	    }
+	}
+    }
 }
